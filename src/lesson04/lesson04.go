331b58007ddc4d7a8c4030faefbb6f71dfed9cac
WakouTTD <t.wakou@gmail.com>
2019-12-25 02:37:53 +0900

    constの使い方

--- src/lesson04/lesson04.go
+++ src/lesson04/lesson04.go
@@ -1,7 +1,22 @@
 package main
 
+import "fmt"
+
+// Pi big P means Public
 const Pi = 3.14
 
-func main() {
+// ユーザ名とパスワード
+const (
+	Username = "test_user"
+	Password = "test_pass"
+)
 
+//int64の最大値を超えると宣言でエラー
+//var big int64 = 9223372036854775807 + 1
+// ところが、const は untype なので宣言時ならint64最大を超えられる
+const big = 9223372036854775807 + 1
+
+func main() {
+	fmt.Println(Pi, Username, Password)
+	fmt.Println(big - 1)
 }
---
+++ tree/myquestion.md
@@ -0,0 +1,18 @@
+
+go でクローラーかスクレイピングする時って何使えば良い？
+
+スライスって何？
+
+goでシステムコマンド呼ぶ時ってcobraってのを使うの？
+
+
+
+gRPC使う
+json.marshall、json.unmarshall使う
+redis
+mysql
+elasticsearch
+
+
+
+
