81307d461572b8c4d586743594273e69b7a7efec
WakouTTD <t.wakou@gmail.com>
2020-01-06 21:56:32 +0900

    lesson継続中　channel前

---
+++ src/07_goroutine/49_goroutine/49_goroutine.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func goroutine(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := 0; i < 5; i++ {
+		//time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func normal(s string) {
+	for i := 0; i < 5; i++ {
+		//time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+// Goroutine
+// 軽量なスレッド、並列処理、他の言語だとマルチプロセス、マルチスレッド、イベント駆動とか言う
+// Goだと暗黙的にやってくれる
+func main() {
+
+	var wg sync.WaitGroup
+	// 1つのsyncグループがありますよという宣言
+	wg.Add(1)
+	go goroutine("world", &wg)
+	normal("hello")
+
+	// 終了するのを待ってやらないと、worldが出力し終わる前に、helloの出力が終わった時点で、
+	// プログラムが終わる。
+	// goroutineの処理が終わらなくても、プログラムの処理自体が終わることがある
+	//time.Sleep(2000 * time.Millisecond)
+	//　そうならないようにするためにsyncのwaitグループがある
+	wg.Wait()
+}
