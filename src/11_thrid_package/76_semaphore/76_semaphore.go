ad8e9ea46d752467125a829f0ea15dfcc470a577
WakouTTD <t.wakou@gmail.com>
2020-01-10 15:58:13 +0900

    lesson継続中 config.ini終了

--- src/11_thrid_package/76_semaphore/76_semaphore.go
+++ src/11_thrid_package/76_semaphore/76_semaphore.go
@@ -8,15 +8,18 @@ import (
 	"golang.org/x/sync/semaphore"
 )
 
+// 何個同時に走らせるか定義、今回は1
 var s *semaphore.Weighted = semaphore.NewWeighted(1)
 
 func longProcess(ctx context.Context) {
+	//
 	isAcquire := s.TryAcquire(1)
 	if !isAcquire {
 		fmt.Println("Could not get lock")
 		return
 	}
 	/*
+		Acquireの英語上の意味は取得
 		if err := s.Acquire(ctx, 1); err != nil{
 			fmt.Println(err)
 			return
@@ -33,6 +36,8 @@ func longProcess(ctx context.Context) {
 // 並列プログラミング環境での複数のプロセスが共有する資源にアクセスするのを制御する際の単純だが便利な抽象化を提供する変数または抽象データ
 // semaphore
 func main() {
+
+	//何するか決まってないコンテキストを作成してgoroutineに渡す
 	ctx := context.TODO()
 	go longProcess(ctx)
 	go longProcess(ctx)
--- src/11_thrid_package/77_config/77_config.go
+++ src/11_thrid_package/77_config/77_config.go
@@ -3,21 +3,26 @@ package main
 import (
 	"fmt"
 
-	"gopkg.in/ini.v1"
+	"github.com/ini"
+	//"gopkg.in/ini.v1"
 )
 
+// ConfigList どんなstructに入れたいか
 type ConfigList struct {
 	Port      int
 	DbName    string
 	SQLDriver string
 }
 
+// Config だよ
 var Config ConfigList
 
+// init main関数が呼ばれる前に実行される
 func init() {
-	cfg, _ := ini.Load("config.ini")
+	cfg, _ := ini.Load("/Users/tateda/work2019/go-study/src/11_thrid_package/77_config/config.ini")
 	Config = ConfigList{
-		Port:      cfg.Section("web").Key("port").MustInt(),
+		Port: cfg.Section("web").Key("port").MustInt(),
+		// confから値が取れなかった場合、MustStringはデフォルト値、Stringは空、を設定
 		DbName:    cfg.Section("db").Key("name").MustString("example.sql"),
 		SQLDriver: cfg.Section("db").Key("driver").String(),
 	}
