4529dc8634dd4edc42d0a8e11d0f1b336328e823
WakouTTD <t.wakou@gmail.com>
2020-01-16 00:41:09 +0900

    関数とメソッド、ポイントレシーバーと値レシーバーについて再読してコメント

--- src/06_struct/39_methods_point_value_reciever/39_methods_point_value_reciever.go
+++ src/06_struct/39_methods_point_value_reciever/39_methods_point_value_reciever.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-// Vertex 大文字のことをキャピタルって言うらしいっす
+// Vertex 大文字のことをキャピタルと言う
 type Vertex struct {
 	// X座標、Y座標
 	X, Y int
 }
 
-// Area 面積　範囲
+// Area 面積　範囲　ただの関数でVertexとの結びつきが無い
 func Area(v Vertex) int {
 	// 縦かける横
 	return v.X * v.Y
@@ -26,12 +26,13 @@ func (v *Vertex) Scale(i int) {
 
 }
 
-// メソッド
+// メソッドとポイントレシーバーと値レシーバー
 func main() {
 
 	v := Vertex{3, 4}
 	fmt.Println(Area(v))
-	//
+	// 上記の書き方だとAreaというfunctionが存在することを知ってなければならない
+	// 下記の書き方だとv.と打った後にArea2のコード補完が出る
 	fmt.Println(v.Area2())
 
 	// 書き換え
