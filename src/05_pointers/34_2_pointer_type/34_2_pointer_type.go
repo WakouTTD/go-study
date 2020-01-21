64a175677e1c087984a6577521172c688f2266db
WakouTTD <t.wakou@gmail.com>
2020-01-22 00:11:11 +0900

    ポインタ型復習

--- src/05_pointers/34_2_pointer_type/34_2_pointer_type.go
+++ src/05_pointers/34_2_pointer_type/34_2_pointer_type.go
@@ -16,13 +16,18 @@ func main() {
 
 	var n = 100
 	one(n)
+	// 参照渡しなので、nに変更は無い
 	fmt.Println(n)
 
+	// ポインタ型を引数にしている場合は、アドレスを渡さなければならないのでアンバサンド
 	two(&n)
+	// ２が表示される
 	fmt.Println(n)
 	fmt.Println("-----------")
+
 	fmt.Println(n)
 	fmt.Println(&n)
 	fmt.Println(*&n)
+	fmt.Println(&*&n)
 
 }
